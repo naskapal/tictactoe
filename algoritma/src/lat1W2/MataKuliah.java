db21e116d14875584791989634ac0e42dad9c772
Rizky Agung Saputro <co.terbang@gmail.com>
2014-10-06 11:01:50 +0800

    w3 practices undone

--- algoritma/src/lat1W2/MataKuliah.java
+++ algoritma/src/lat1W2/MataKuliah.java
@@ -1,6 +1,34 @@
 package lat1W2;
 
 public class MataKuliah {
-
+	int kode;
 	String namaMatkul;
+	int jmlSKS;
+	int semester;
+	
+	MataKuliah(){
+		kode = 0;
+		namaMatkul = "";
+		jmlSKS = 0;
+		semester = 0;
+	}
+	
+	MataKuliah(int kodeMatkul,String nama, int SKS, int smt){
+		kode = kodeMatkul;
+		namaMatkul = nama;
+		jmlSKS = SKS;
+		semester = smt;
+	}
+	public void setKode(int kodeMatkul){
+		kode = kodeMatkul;
+	}
+	public void setNama(String nama){
+		namaMatkul = nama;
+	}
+	public void setSKS(int SKS){
+		jmlSKS = SKS;
+	}
+	public void setSemester(int smt){
+		semester = smt;
+	}
 }
--- algoritma/src/lat1W2/MatkulExam.java
+++ algoritma/src/lat1W2/MatkulExam.java
@@ -2,4 +2,35 @@ package lat1W2;
 
 public class MatkulExam extends MataKuliah {
 
+	double tugas;
+	double quiz;
+	double uts;
+	double uas;
+	double absen;
+	
+	MatkulExam(){
+		tugas = 0;
+		quiz = 0;
+		uts = 0;
+		uas = 0;
+		absen = 0;
+	}
+	MatkulExam(int kode, String nama, int SKS, int semester){
+	}
+	MatkulExam(double nilaiTugas, double nilaiQuiz, double nilaiUts, double nilaiUas, double nilaiAbsen){
+		tugas = nilaiTugas;
+		quiz = nilaiQuiz;
+		uts = nilaiUts;
+		uas = nilaiUas;
+		absen = nilaiAbsen;
+	}
+	private double nilaiAkhir(){
+		return ((tugas*0.10)+(quiz*0.15)+(uts*0.30)+(uas*0.30)+(absen*0.15));
+	}
+	public String kelulusan(){
+		if (nilaiAkhir() >= 56)
+			return "LULUS";
+		else
+			return "TIDAK LULUS";
+	}
 }
