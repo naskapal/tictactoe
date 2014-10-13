0c9ee1aadaf9524f7c840309b17af9b9e7580bc0
Rizky Agung Saputro <co.terbang@gmail.com>
2014-10-13 09:51:17 +0800

    starting w4 task

--- algoritma/w3/lat3Soal/Angka.java
+++ algoritma/w3/lat3Soal/Angka.java
@@ -9,7 +9,7 @@ public class Angka {
 	}
 	Angka(int size){
 		banyak = size;
-		bilangan = new int[banyak];
+		bilangan = new int[size];
 	}
 	public boolean cekAngka(int nilai){
 		for (int i = 0; i < bilangan.length; i++){
@@ -25,5 +25,6 @@ public class Angka {
 		for (int i = 0; i < bilangan.length; i++){
 			System.out.print(bilangan[i]);
 		}
+		System.out.println();
 	}
 }
---
+++ algoritma/w3/lat3Soal/TestClass.java
@@ -0,0 +1,9 @@
+package lat3Soal;
+
+public class TestClass {
+
+	public static void main(String[] args){
+		Angka a1 = new Angka();
+		a1.printIsi();
+	}
+}
